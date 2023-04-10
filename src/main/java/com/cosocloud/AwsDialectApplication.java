5fca0f6cdaed16f3bc384b12f83d253e04c9217d
hemantm <hemantm@hyperlinkinfosystem.net.in>
2023-04-10 13:25:53 +0530

    Added support for multiple files and auto exit once processed

--- src/main/java/com/cosocloud/AwsDialectApplication.java
+++ src/main/java/com/cosocloud/AwsDialectApplication.java
@@ -2,13 +2,43 @@ package com.cosocloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.FileTemplateResolver;
+
+import com.cosocloud.dialect.AWSDialect;
+import com.cosocloud.dialect.TestDialect;
 
 @SpringBootApplication
 public class AwsDialectApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AwsDialectApplication.class, args);
-		
-	}
+		ConfigurableApplicationContext ctx = SpringApplication.run(AwsDialectApplication.class, args);
+		 TemplateEngine templateEngine = new TemplateEngine();
+	        FileTemplateResolver fileTemplateResolver = new FileTemplateResolver();
+	        // uncomment these to run in local
+//	        fileTemplateResolver.setPrefix("./");
+//	        fileTemplateResolver.setSuffix(".txt");
+	        fileTemplateResolver.setTemplateMode(TemplateMode.TEXT);
+	        templateEngine.setTemplateResolver(fileTemplateResolver);
+	        templateEngine.addDialect(new AWSDialect());
+
+	        Context context = new Context();
+	        context.setVariable("name", System.getProperty("name"));
+	        
+	        for (int i = 0; i < args.length; i++) {
+	        	String output = templateEngine.process(args[i], context);
+	        	System.out.println("=============================");
+		        System.out.println("Output for File:"+(i+1));
+		        System.out.println("=============================");
+	        	System.out.println(output);
+	        	System.out.println("\n\n\n\n");
+			}
+	        
+	        
+	        SpringApplication.exit(ctx, ()-> 0);
+	        }
 
 }
--- src/main/java/com/cosocloud/ThymeLeafTest.java
+++ src/main/java/com/cosocloud/ThymeLeafTest.java
@@ -1,24 +1,24 @@
-package com.cosocloud;
-import org.thymeleaf.TemplateEngine;
-import org.thymeleaf.context.Context;
-import org.thymeleaf.templatemode.TemplateMode;
-import org.thymeleaf.templateresolver.FileTemplateResolver;
-
-import com.cosocloud.dialect.TestDialect;
-
-public class ThymeLeafTest {
-    public static void main(String [] args) {
-        TemplateEngine templateEngine = new TemplateEngine();
-        FileTemplateResolver fileTemplateResolver = new FileTemplateResolver();
-        fileTemplateResolver.setPrefix("./");
-        fileTemplateResolver.setSuffix(".txt");
-        fileTemplateResolver.setTemplateMode(TemplateMode.TEXT);
-        templateEngine.setTemplateResolver(fileTemplateResolver);
-        templateEngine.addDialect(new TestDialect());
-
-        Context context = new Context();
-        context.setVariable("name", System.getProperty("name"));
-        String output = templateEngine.process("hello", context);
-        System.out.println(output);
-    }
-}
+//package com.cosocloud;
+//import org.thymeleaf.TemplateEngine;
+//import org.thymeleaf.context.Context;
+//import org.thymeleaf.templatemode.TemplateMode;
+//import org.thymeleaf.templateresolver.FileTemplateResolver;
+//
+//import com.cosocloud.dialect.TestDialect;
+//
+//public class ThymeLeafTest {
+//    public static void main(String [] args) {
+//        TemplateEngine templateEngine = new TemplateEngine();
+//        FileTemplateResolver fileTemplateResolver = new FileTemplateResolver();
+//        fileTemplateResolver.setPrefix("./");
+//        fileTemplateResolver.setSuffix(".txt");
+//        fileTemplateResolver.setTemplateMode(TemplateMode.TEXT);
+//        templateEngine.setTemplateResolver(fileTemplateResolver);
+//        templateEngine.addDialect(new TestDialect());
+//
+//        Context context = new Context();
+//        context.setVariable("name", System.getProperty("name"));
+//        String output = templateEngine.process("hello", context);
+//        System.out.println(output);
+//    }
+//}
