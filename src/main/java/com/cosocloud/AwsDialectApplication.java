d1c228fa695f9e8b2aa9e0f2bf0443d7019fded2
Richard Levenberg <richard.levenberg@cosocloud.com>
2023-04-10 19:05:29 -0700

    bannerMode off using SpringApplicationBuilder; removed System.out output

--- src/main/java/com/cosocloud/AwsDialectApplication.java
+++ src/main/java/com/cosocloud/AwsDialectApplication.java
@@ -1,6 +1,8 @@
 package com.cosocloud;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.Banner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.thymeleaf.TemplateEngine;
@@ -15,30 +17,29 @@ import com.cosocloud.dialect.TestDialect;
 public class AwsDialectApplication {
 
 	public static void main(String[] args) {
-		ConfigurableApplicationContext ctx = SpringApplication.run(AwsDialectApplication.class, args);
-		 TemplateEngine templateEngine = new TemplateEngine();
-	        FileTemplateResolver fileTemplateResolver = new FileTemplateResolver();
-	        // uncomment these to run in local
-//	        fileTemplateResolver.setPrefix("./");
-//	        fileTemplateResolver.setSuffix(".txt");
-	        fileTemplateResolver.setTemplateMode(TemplateMode.TEXT);
-	        templateEngine.setTemplateResolver(fileTemplateResolver);
-	        templateEngine.addDialect(new AWSDialect());
+		ConfigurableApplicationContext ctx = new SpringApplicationBuilder(AwsDialectApplication.class).bannerMode(Banner.Mode.OFF).run(args);
+        TemplateEngine templateEngine = new TemplateEngine();
+        FileTemplateResolver fileTemplateResolver = new FileTemplateResolver();
+        // uncomment these to run in local
+        // fileTemplateResolver.setPrefix("./");
+        // fileTemplateResolver.setSuffix(".txt");
+        fileTemplateResolver.setTemplateMode(TemplateMode.TEXT);
+        templateEngine.setTemplateResolver(fileTemplateResolver);
+        templateEngine.addDialect(new AWSDialect());
 
-	        Context context = new Context();
-	        context.setVariable("name", System.getProperty("name"));
-	        
-	        for (int i = 0; i < args.length; i++) {
-	        	String output = templateEngine.process(args[i], context);
-	        	System.out.println("=============================");
-		        System.out.println("Output for File:"+(i+1));
-		        System.out.println("=============================");
-	        	System.out.println(output);
-	        	System.out.println("\n\n\n\n");
-			}
-	        
-	        
-	        SpringApplication.exit(ctx, ()-> 0);
-	        }
+        Context context = new Context();
+        context.setVariable("name", System.getProperty("name"));
 
+        for (int i = 0; i < args.length; i++) {
+            String output = templateEngine.process(args[i], context);
+            //System.out.println("=============================");
+            //System.out.println("Output for File:"+(i+1));
+            //System.out.println("=============================");
+            System.out.println(output);
+            //System.out.println("\n\n\n\n");
+        }
+
+
+        SpringApplication.exit(ctx, ()-> 0);
+    }
 }
